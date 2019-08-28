754fe8383824e4cca139fc97e675caea08554ed9
porowns <porowns@gmail.com>
2019-08-28 10:25:43 -0400

    Significant improvement in speed using Popen

--- utilities/bash.py
+++ utilities/bash.py
@@ -1,41 +1,40 @@
-import subprocess
+from concurrent.futures import ThreadPoolExecutor
+from subprocess import Popen  # ended up being way faster than subprocess.run
 
 
 def bash_upload_file(user, host, absolute_remote_directory, local_file, remote_file):
-    respnse = subprocess.run(
-        ['ssh', '%s@%s' % (user, host), 'mkdir', '-p',
-         absolute_remote_directory]
-    )
-    response = subprocess.run(
-        ['scp', local_file, "%s@%s:%s/%s" %
-            (user, host, absolute_remote_directory, remote_file)]
-    )
+    p1 = Popen(['scp', local_file, "%s@%s:%s/%s" %
+                (user, host, absolute_remote_directory, remote_file)])
+    return [p1]
 
 
 def bash_convert_file_to_encoding(user, host, absolute_remote_directory, remote_file, encoding):
-    response = subprocess.run(
+    p1 = Popen(
         ['ssh', '%s@%s' % (user, host), 'iconv', '-f',
          "IBM-1047", '-t', encoding, "%s/%s.temp" % (absolute_remote_directory, remote_file), ">", "%s/%s" % (absolute_remote_directory, remote_file)]
     )
-    response = subprocess.run(
+    p2 = Popen(
         ['ssh', '%s@%s' % (user, host), 'chtag', '-tc', encoding, "%s/%s" %
          (absolute_remote_directory, remote_file)],
     )
-    response = subprocess.run(
+    p3 = Popen(
         ['ssh', '%s@%s' % (user, host), 'rm', "%s/%s.temp" %
          (absolute_remote_directory, remote_file)],
     )
+    return [p1, p2, p3]
 
 
 def bash_remove_file(user, host, absolute_remote_directory, remote_file):
-    response = subprocess.run(
+    p1 = Popen(
         ['ssh', '%s@%s' % (user, host), 'rm', "%s/%s" %
          (absolute_remote_directory, remote_file)], stdout=FNULL
     )
+    return [p1]
 
 
 def bash_tag_file(user, host, absolute_remote_directory, remote_file, encoding):
-    response = subprocess.run(
+    p1 = Popen(
         ['ssh', '%s@%s' % (user, host), 'chtag', '-tc', encoding, "%s/%s" %
          (absolute_remote_directory, remote_file)],
     )
+    return [p1]
